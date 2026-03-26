b0ac14eb217f09492241c4054b70c1f2934cbc9f
kezzzzzqwqw <keziakeilanni@gmail.com>
2026-03-26 09:43:04 +0800

    W04 Assignment: YouTube Video Program

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
+        Video video1 = new Video("C# Basics Tutorial", "John Doe", 330);
+        Comment comment1 = new Comment("Alice", "Great Tutorial!");
+        Comment comment2 = new Comment("Bob", "Very helpful, thanks!" );
+        Comment comment3 = new Comment("Charlie", "I learned a lot.");
+
+        video1.AddComment(comment1);
+        video1.AddComment(comment2);
+        video1.AddComment(comment3);
+
+        video1.DisplayInfo();
+
+
     }
 }
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,43 @@
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return _comments.Count();
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+
+        int minutes = _length / 60;
+        int seconds = _length % 60;
+        Console.WriteLine($"Length: {minutes}:{seconds:D2}");
+
+        Console.WriteLine($"Number of Comments: {GetCommentCount()}");
+        Console.WriteLine("Comments:");
+
+        foreach (Comment comment in _comments)
+        {
+            comment.Display();
+        }
+
+        Console.WriteLine();
+    }
+}
